371809c495461711e334cd89f19d2ec560271dd2
DiniduMadushan <dinidumadushan15576@gmail.com>
2024-05-05 03:19:14 -0400

    practise activity

--- controlFlow/controlFlow.js
+++ controlFlow/controlFlow.js
@@ -49,3 +49,20 @@ let isAuthenticated=true;
 let authenticationStatus=isAuthenticated? "Authenticated":"Not Authenticated";
 
 console.log("Authentication status: ",authenticationStatus);
+
+//practise task
+
+let user_type="employee";
+let access;
+
+if(user_type==="empolyee"){
+    access="Dietary services";
+}else if(user_type==="enrolled member"){
+    access="Dietary services and one-on-one interaction";
+}else if(user_type==="subscriber"){
+    access=" partial access toDietary services";
+}else{
+    access="please enroll or at least subscribe first";
+}
+
+console.log("access to dietary services: ",access);
